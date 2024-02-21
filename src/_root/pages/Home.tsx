40795d317b55a90699330716a8e929664bc1428e
ImpactFurax <bortjean@yahoo.fr>
2024-02-21 15:17:12 +0100

    update

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,10 +1,9 @@
-import Carroussel from "@/components/shared/Carroussel";
-import { Slides } from "@/constants";
-
 const Home = () => {
   return (
-    <section className="w-full m-auto relative group">
-      <Carroussel slides={Slides} />
+    <section className="relative size-full">
+      <video preload="auto" loop muted autoPlay className="w-full min-h-[calc(100vh-257px)] md:min-h-[calc(100vh-156px)] object-cover">
+        <source src="/assets/videos/accueil.mp4" type="video/mp4" />
+      </video>
     </section>
   );
 }
